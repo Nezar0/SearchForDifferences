df10f13f29e0d3ccdcda360ba4e9a3fe8febbf52
Nezar0 <8952905o213@gmail.com>
2021-06-01 17:42:27 +0700

    .

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private Text countText;
+    [SerializeField] private int total;
+    private int foundDiff;
+
+    private void Start()
+    {
+        countText.text = $"{foundDiff}/{total}";
+    }
+
+    public void WriteCount()
+    {
+        foundDiff++; 
+        countText.text = $"{foundDiff}/{total}";
+    }
+}
---
+++ Assets/Scripts/FoundObj.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FoundObj : MonoBehaviour
+{
+    private Button btn;
+    private Image img;
+    [SerializeField] private Counter counter;
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        img = GetComponent<Image>();
+    }
+    public void Found()
+    {
+        counter.WriteCount();
+        img.color = Color.red;
+        btn.enabled = false;
+    }
+}
